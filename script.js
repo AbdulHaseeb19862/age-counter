18547a38205aeb8f9cfd01ebfd52bc6aa7949dc9
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-09-29 12:51:25 +0500

    Get three boxes for showing years,months and days

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const button = document.querySelector("button");
 button.addEventListener("click", function () {
+  const years = document.getElementById("years");
+  const months = document.getElementById("months");
+  const days = document.getElementById("days");
   const input = document.querySelector("input");
   const inputValue = new Date(input.value);
   const currentDate = new Date();
   let ageYears = currentDate.getFullYear() - inputValue.getFullYear();
   let ageMonths = currentDate.getMonth() - inputValue.getMonth();
   let ageDays = currentDate.getDate() - inputValue.getDate();
+
+  
 });
